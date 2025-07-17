966b70399da5a82f4cf8540b8c255d6c3868d9f3
samiksha3002 <Samiksha.jaiswal.bca@srwc.raisoni.net>
2025-07-17 15:39:00 +0530

    pirbs

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header className="w-full py-4 bg-white shadow-md flex justify-center items-center">
       <div className="flex items-center gap-2">
-        <Image src="/logo.jpg" alt="TalknType Logo" width={40} height={40} />
+        <Image src="/logop.png" alt="TalknType Logo" width={40} height={40} />
         <h1 className="text-xl font-semibold text-gray-800">TalknType</h1>
       </div>
     </header>
