975ab51f0b05a67eef56fefc5c9a0e3328a520ac
samiksha3002 <Samiksha.jaiswal.bca@srwc.raisoni.net>
2025-07-17 15:46:11 +0530

    jshsgd

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
   return (
     <header className="w-full py-4 bg-white shadow-md flex justify-center items-center">
       <div className="flex items-center gap-2">
-        <Image src="/logop.png" alt="TalknType Logo" width={40} height={40} />
-        <h1 className="text-xl font-semibold text-gray-800">TalknType</h1>
+        <Image src="/logop.png" alt="TalknType Logo" width={80} height={60} />
+        <h1 className="text-xl font-semibold text-gray-800"></h1>
       </div>
     </header>
   );
