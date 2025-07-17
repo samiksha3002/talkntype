b9abcb3dd29a83638cceed43f741d041d140fc5c
samiksha3002 <Samiksha.jaiswal.bca@srwc.raisoni.net>
2025-07-17 22:33:09 +0530

    rtyu

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import React from "react";
-import Header from "../components/Header";
+
 import Hero from "../components/Hero";
+import Header from "../components/Header";
 
 export default function App() {
   return (
